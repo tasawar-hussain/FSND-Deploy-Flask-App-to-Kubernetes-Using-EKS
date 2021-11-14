bdd54671a5d7da08b131fbba60dee7ae06729b3c
Tasawar Hussain <31658686+tasawar-hussain@users.noreply.github.com>
2021-11-15 02:36:39 +0500

    Add test in pre build

--- test_main.py
+++ test_main.py
@@ -31,10 +31,14 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+
+def test_ci(client):
+    assert 2 + 3 == 4
